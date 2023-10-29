28331d801e458a4789ec4079892e010edda55e00
Chibueze10 <chibuezegeoffrey@gmail.com>
2023-10-29 23:48:29 +0100

    Changes

---
+++ TechDaily.Apllication/Interfaces/IAuthorService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using TechDaily.Common.DTOs.Requests;
+using TechDaily.Common.DTOs.Responses;
+using TechDaily.Domain.Entities;
+
+namespace TechDaily.Apllication.Interfaces
+{
+    public interface IAuthorService
+    {
+        Task<ApiResponse> AddAsync(AuthorRequestDto request);
+        ApiResponse<PaginatedResponse<AuthorResponseDto>> GetAllAuthors(int pageSize, int pageNumber);
+        ApiResponse<PaginatedResponse<AuthorResponseDto>> UpdateAuthor(int pageSize, int pageNumber);
+    }
+}
---
+++ TechDaily.Apllication/Interfaces/ICategoryService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using TechDaily.Common.DTOs.Responses;
+
+namespace TechDaily.Apllication.Interfaces
+{
+    public interface ICategoryService
+    {
+      
+    }
+}
